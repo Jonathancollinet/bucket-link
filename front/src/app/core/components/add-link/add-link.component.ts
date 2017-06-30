ae7ce057787a5155171e85433702c0ec170b99a9
Luc Terracher <lucterracher@gmail.com>
2017-06-30 23:02:51 +0200

    add sidebar

---
+++ front/src/app/core/components/add-link/add-link.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'core-add-link',
+  templateUrl: './add-link.component.html',
+  styleUrls: ['./add-link.component.scss']
+})
+export class AddLinkComponent {
+
+  constructor() {}
+
+  
+}
