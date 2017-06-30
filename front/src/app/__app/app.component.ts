fb532e11c6bf306c7d7644400fde7df5cd0ab3b5
Luc Terracher <lucterracher@gmail.com>
2017-06-30 23:59:30 +0200

    add responsive

--- front/src/app/__app/app.component.ts
+++ front/src/app/__app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,48 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   private _opened: boolean = false;
+  public _layout = 0;
+  private _closeOnClickOutside: boolean = false;
+  private _modeNum: number = 0;
+  public minimalWidth = "601px";
+
+  constructor(private _zone: NgZone) {
+    this.enableResponsive();
+  }
+
+  private enableResponsive(): void {
+    let mq = window.matchMedia(`screen and (min-width: ${this.minimalWidth})`);
+    // init
+    mq.matches ? this.desktopMode() : this.mobileMode();
+    // listen for change
+    mq.addListener((matchMedia) => {
+      this._zone.run(() => { // force update
+        matchMedia.matches ? this.desktopMode() : this.mobileMode();
+      });
+    });
+  }
+
+   private desktopMode(): void {
+    this._openSidebar();
+    this._modeNum = 1;
+    this._closeOnClickOutside = false;
+    this._layout = 0;
+  }
+
+  private mobileMode(): void {
+    this._closeSidebar();
+    this._modeNum = 0;
+    this._closeOnClickOutside = true;
+    this._layout = 1;
+  }
+
+  private _closeSidebar(): void {
+    this._opened = false;
+  }
+
+  private _openSidebar(): void {
+    this._opened = true;
+  }
 
   private _toggleSidebar() {
     this._opened = !this._opened;
