54284532f72cbfde51e3bb26236616a74db86e74
Luc Terracher <lucterracher@gmail.com>
2017-07-09 12:35:48 +0200

    add beforUpdate hook for future password update + continue admin route

--- api/app/v1/admin/admin.js
+++ api/app/v1/admin/admin.js
@@ -80,43 +80,107 @@ module.exports = {
   // #### END GET // BEGIN POST ####
 
   async createUser(req, res) {
-  
+    const user = await User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+    setResponse(res, 'OK', user)
   },
 
   async createBucket(req, res) {
-  
+    const bucket = await Bucket.create({
+      name: req.body.name,
+      color: req.body.color,
+      userId: req.body.userId
+    })
+    setResponse(res, 'OK', bucket)
   },
 
   async createLink(req, res) {
-  
+    const link = await Link.create({
+      title: req.body.title,
+      description: req.body.description,
+      url: req.body.url,
+      color: req.body.color,
+      userId: req.body.userId,
+      bucketId: req.body.bucketId
+    })
+    setResponse(res, 'OK', link)
   },
 
   // #### END POST // BEGIN PATCH ####
 
   async updateUserByID(req, res) {
-  
+    const user = await User.fineOne({
+      where: { id: req.params.id }
+    })
+    if (!user) {
+      setResponse(res, 'NOT_FOUND')
+    } else {
+      user = await user.update({
+        email: req.body.email,
+        password: req.body.password
+      })
+      setResponse(res, 'OK', user)
+    }
   },
 
   async updateBucketByID(req, res) {
-  
+    const bucket = await Bucket.fineOne({
+      where: { id: req.params.id }
+    })
+    if (!bucket) {
+      setResponse(res, 'NOT_FOUND')
+    } else {
+      bucket = await bucket.update({
+        name: req.body.name,
+        color: req.body.color,
+        userId: req.body.userId
+      })
+      setResponse(res, 'OK', bucket)
+    }
   },
 
   async updateLinkByID(req, res) {
-  
+    const link = await Link.fineOne({
+      where: { id: req.params.id }
+    })
+    if (!link) {
+      setResponse(res, 'NOT_FOUND')
+    } else {
+      link = await link.update({
+        title: req.body.title,
+        description: req.body.description,
+        url: req.body.url,
+        color: req.body.color,
+        userId: req.body.userId,
+        bucketId: req.body.bucketId
+      })
+      setResponse(res, 'OK', link)
+    }
   },
 
   // #### END PATCH // BEGIN DELETE ####
 
   async deleteUserByID(req, res) {
-  
+    const user = await User.destroy({
+      where: { id: req.params.id }
+    })
+    setResponse(res, 'OK', user)
   },
 
   async deleteBucketByID(req, res) {
-  
+    const bucket = await Bucket.destroy({
+      where: { id: req.params.id }
+    })
+    setResponse(res, 'OK', bucket)
   },
 
   async deleteLinkByID(req, res) {
-  
+    const link = await Link.destroy({
+      where: { id: req.params.id }
+    })
+    setResponse(res, 'OK', link)
   }
 
   // #### END ####
