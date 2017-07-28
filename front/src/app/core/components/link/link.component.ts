a7e94dfb7ba187cddaeb6d211b6ede79a7b3f764
Luc Terracher <lucterracher@gmail.com>
2017-07-28 16:14:01 +0200

    add edit-link, fix css for long bucket name

--- front/src/app/core/components/link/link.component.ts
+++ front/src/app/core/components/link/link.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import * as moment from 'moment';
 
 import { Link } from '../../models';
+import { BucketService } from '../../services/bucket.service';
 import { ToastService } from '../../services/toast.service';
 
 @Component({
@@ -14,6 +16,8 @@ import { ToastService } from '../../services/toast.service';
 export class LinkComponent {
 
   private _link: Link;
+  private _mode: number = 0;
+  private editLinkForm: any
 
   @Input()
   set link(link: Link) {
@@ -25,7 +29,25 @@ export class LinkComponent {
     }
   }
 
-  constructor(private _router: Router, private _toast: ToastService) {}
+  @Output() linkEvent = new EventEmitter();
+
+  constructor(
+    private _router: Router,
+    private _toast: ToastService,
+    private _bucket: BucketService,
+    private _fb: FormBuilder
+  ) {}
+
+  public toggleMode(): void {
+    // VIEW // EDIT
+    this._mode = this._mode === 0 ? 1 : 0;
+    if (this._mode === 1) {
+      this.editLinkForm = this._fb.group({
+        'alt_title': [''],
+        'alt_description': ['']
+      });
+    }
+  }
 
   public formatDate(link: Link): Link {
       if (link.setCreated && link.setUpdated) {
@@ -37,6 +59,34 @@ export class LinkComponent {
      
       link.dateHasBeenFormated = true;
       return link;
+  }
+
+  public editLink(formData, valid: boolean): void {
+    if (valid) {
+      if (formData.alt_title.trim() || formData.alt_description.trim()) {
+        let tmp = {
+          alt_title: (formData.alt_title || this._link.title),
+          alt_description: (formData.alt_description || this._link.description)
+        };
+        this._bucket.patchLink(this._link.id, tmp).subscribe((resp) => {
+          this.linkEvent.emit(true);
+          this.editLinkForm.controls.alt_title.updateValueAndValidity(formData.alt_title);
+          this.editLinkForm.controls.alt_description.updateValueAndValidity(formData.alt_description);
+        }, (err) => {
+          console.error(err);
+        });
+      } else {
+        this.toggleMode();
+      }
+    }
+  }
+
+   public deleteLink(id: number): void {
+    this._bucket.deleteLink(id).subscribe((resp) => {
+      this.linkEvent.emit(true);
+    }, (err) => {
+      console.error('delete bucket', err);
+    })
   }
 
   public displayConfirmCopy() {
