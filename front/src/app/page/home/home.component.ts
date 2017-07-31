8bc9db50a3ebc32ab131ba478db3956b3e5a9180
jonathan collinet <john.collinet@gmail.com>
2017-07-31 23:20:42 +0200

    wip topbar landing page and little things

--- front/src/app/page/home/home.component.ts
+++ front/src/app/page/home/home.component.ts
@@ -10,12 +10,26 @@ import { AuthService } from '../../core';
 })
 export class HomeComponent implements AfterViewInit {
 
+  private _stepper: number = 0;
+
   constructor(private router: Router, private _auth: AuthService) {
     if (this._auth.isLoggedIn()) this.router.navigate(['/login']);
   }
 
   ngAfterViewInit() {
     if (this._auth.isLoggedIn()) this.router.navigate(['/login']);
+  }
+
+  decrementStep() {
+    if (this._stepper > 0) {
+      this._stepper -= 1;
+    }
+  }
+
+  incrementStep() {
+    if (this._stepper < 4) {
+      this._stepper += 1;
+    }
   }
 
 }
